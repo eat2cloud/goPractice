88cf02379bbeb31eefd83397dd08461bfd754158
evan wu <evwu@ebay.com>
2022-11-18 17:19:13 +0800

    practice 01

--- study/basic/data_type/struct/anonymous.go
+++ study/basic/data_type/struct/anonymous.go
@@ -2,16 +2,30 @@ package main
 
 import "fmt"
 
-func structAnonymous() {
+func anonymousStruct() {
 	var user struct {
 		Name string
 		Age  int
 	}
-	user.Name = "小王子"
+	user.Name = "哒哒"
 	user.Age = 18
 	fmt.Printf("%#v\n", user)
 }
 
+// Person 结构体Person类型
+type Person struct {
+	string
+	int
+}
+
 func main() {
-	structAnonymous()
+	//anonymousStruct()
+
+	p1 := Person{
+		"丁丁",
+		12,
+	}
+	//fmt.Printf("p1: %#v\n", p1)
+	fmt.Println(p1.string)
+	fmt.Println(p1.int)
 }
